57500d1078f39f380a35dff4989ff752250c6e99
Connie Hyman <constance.hyman@mchsi.com>
2019-01-09 20:45:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
 		user = User.new(username: params['username'], password: params['password'])
 
 		if user.save
@@ -32,7 +31,6 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
 		user = User.find_by(username: params['username'])
 		if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
